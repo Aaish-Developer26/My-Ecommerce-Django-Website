b520f814a056f8a13a6a4a127e4d36f5dded2154
Aaish-Developer26 <aaishaashu2626@gmail.com>
2025-02-23 03:11:22 +0500

    second commit

--- app/urls.py
+++ app/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from app import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from .forms import LoginForm
+
 urlpatterns = [
     
     path('',views.ProductView.as_view(),name="home"),
     path('product-detail/<int:pk>', views.ProductDetail.as_view(), name='product-detail'),
-
-
     path('cart/', views.add_to_cart, name='add-to-cart'),
     path('buy/', views.buy_now, name='buy-now'),
     path('profile/', views.profile, name='profile'),
@@ -15,7 +16,8 @@ urlpatterns = [
     path('orders/', views.orders, name='orders'),
     path('changepassword/', views.change_password, name='changepassword'),
     path('mobile/', views.mobile, name='mobile'),
-    path('login/', views.login, name='login'),
-    path('registration/', views.customerregistration, name='customerregistration'),
+    path('mobile/<slug:data>', views.mobile, name='mobiledata'),
+    path('accounts/login/',auth_views.LoginView.as_view(template_name='app/login.html',authentication_form=LoginForm),name='login'),
     path('checkout/', views.checkout, name='checkout'),
+    path('registration/',views.CustomerRegisterationView.as_view(),name="customerregistration")
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
